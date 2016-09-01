e1db58f635ab3b16cf6e1be731e302e4a96b4384
Amir Zakariaei <amirzakaria110@gmail.com>
2016-09-01 17:18:55 +0430

    esme avalin commit e man
    tozihe avalin commit e man

---
+++ components/netbarg/2016-09-01-4ta-field-tahe-netbarg.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE `netbarg` ADD `seller_tell_mobayl` TEXT NOT NULL AFTER `thelastone`, ADD `site_addres` TEXT NOT NULL AFTER `seller_tell_mobayl`, ADD `tel_addres` TEXT NOT NULL AFTER `site_addres`, ADD `insta_addres` TEXT NOT NULL AFTER `tel_addres`;
+
