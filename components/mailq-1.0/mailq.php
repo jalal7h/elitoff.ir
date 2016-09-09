eab64b33aab16faafa16d3eb46803a2098cf0427
Jalal Hoseini <jalal7h@gmail.com>
2016-09-09 17:50:37 +0430

    mailq array support

--- components/mailq-1.0/mailq.php
+++ components/mailq-1.0/mailq.php
@@ -4,18 +4,35 @@
 # its storing mail in queue, and sends it via cron
 
 function mailq( $to , $subject , $text , $from , $html=true ){
-	if($html){
+	
+	if( is_array($to) ){
+		if( sizeof($to) ){
+			foreach ($to as $i => $this_mail) {
+				mailq( $this_mail , $subject , $text , $from , $html );
+			}
+		}
+		return true;
+	}
+
+	if( $html ){
 		$html = 1;
 	} else {
 		$html = 0;
 	}
+
 	$text = mysql_real_escape_string($text);
+	
 	if(!dbq(" INSERT INTO `mailq` (`to`,`subject`,`text`,`mail_from`,`html`) VALUES ('$to','$subject','$text','$from','$html') ")){
 		e("error on mailq - ".__LINE__);
 		e(dbe());
 		return false;
+	
 	} else {
 		return true;
 	}
+
 }
+
+
+
 
