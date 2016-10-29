71d0e6489e70eddd8328cf1fd861e4e1fd615e6b
Jalal Hoseini <jalal7h@gmail.com>
2016-10-29 21:33:22 +0330

    fix n2 - copon code md5x

--- components/netbarg/nb_management_copon.php
+++ components/netbarg/nb_management_copon.php
@@ -1,21 +1,27 @@
 <?
 
 function nb_management_copon(){
-	if(!$id = $_REQUEST['id']){
+	
+	if(! $id = $_REQUEST['id'] ){
 		return false;
-	} else if(!$rw = table("netbarg", $id)){
+	
+	} else if(! $rw = table("netbarg", $id) ){
 		return false;
-	} // else
+	}
 	
 	echo "<div class='nb_management_copon'>";
-	echo "<a href='".link_netbarg($rw)."' class='title'>همه کوپن های مربوط به :‌ ".$rw['name']."</a>";
+
+	echo "<div class='title' ><a href='".link_netbarg($rw)."' >همه کوپن های مربوط به :‌ ".$rw['name']."</a></div>";
 	$url = "./?page=admin&cp=".$_REQUEST['cp']."&func=".$_REQUEST['func']."&do=".$_REQUEST['do']."&id=".$id;
 	$menu = array(
 		"nb_management_copon_list" => "لیست کوپن های فعال",
 		"nb_management_copon_archive" => "لیست کوپن های فروخته شده",
 		"nb_management_copon_form" => "ثبت کوپن جدید",
 	);
+
 	listmaker_tabmenu( $menu , $url , $func_k="func2" );
+
 	echo "</div>";
+
 }
 
--- components/netbarg/nb_management_copon_form_generate.php
+++ components/netbarg/nb_management_copon_form_generate.php
@@ -11,11 +11,14 @@ function nb_management_copon_form_generate( $count , $netbarg_id=null ){
 	if(!$count = intval($count)){
 		return false;
 	} else {
+		
 		$nbccc = dbr(dbq(" SELECT COUNT(*) FROM `netbarg_copon` WHERE `netbarg_id`='$netbarg_id' "), 0, 0);
 		$_SESSION['nb-copon-print-list'] = null;
-		for($i=1; $i<=$count; $i++){
+		
+		for( $i=1; $i<=$count; $i++ ){
 			$_SESSION['nb-copon-print-list'][] = nb_management_copon_form_generate_this( $netbarg_id , $nbccc+$i );
 		}
+
 		echo "
 		<div class='nb_management_copon_form_generate'>
 			<div class='text'>$count کوپن صادر شد.</div>
@@ -26,12 +29,16 @@ function nb_management_copon_form_generate( $count , $netbarg_id=null ){
 }
 
 function nb_management_copon_form_generate_this( $netbarg_id , $nbccc ){
-	$code = nb_netbarg_id_encode($netbarg_id)."-".($nbccc+1000);
-	if(!dbq(" INSERT INTO `netbarg_copon` (`netbarg_id`,`code`) VALUES ('$netbarg_id','$code') ")){
+	
+	$code = nb_netbarg_id_encode($netbarg_id)."-".md5x( $nbccc+1000, 6 );
+
+	if(! dbq(" INSERT INTO `netbarg_copon` (`netbarg_id`,`code`) VALUES ('$netbarg_id','$code') ") ){
 		echo("nb_management_copon_form_generate_this - ".__LINE__);
 		return false;
+
 	} else {
 		return $code;
 	}
+
 }
 
